0c648d28c07b86519f4e05caf08222828cc2da8a
NesCarr <79880182+NesCarr@users.noreply.github.com>
2022-03-27 11:33:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 function calcular(oper) {
   var valor1 = document.calcform.valor1.value
   var valor2 = document.calcform.valor2.value
-
   if (oper == 'somar') {
-    var res = parseInt(valor1) + parseInt(valor2)
+    var res = parseFloat(valor1) + parseFloat(valor2)
   } else {
     if (oper == 'subtrair') {
       var res = valor1 - valor2
